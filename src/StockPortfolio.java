5b81e5aeaafebca5393386bfa08fc2a6451ed15b
Luke-Quarderer <lquarderer13@gmail.com>
2023-07-25 10:31:26 +1000

    Final Product

---
+++ src/StockPortfolio.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class StockPortfolio {
+    
+    private HashMap<Stock, Integer> portfolio = new HashMap<>();
+
+    public StockPortfolio(){}
+
+    public void addStock(Stock stock, int n){
+        portfolio.put(stock, n);
+    }
+
+    public void viewPortfolio(){
+        for(Stock stock : portfolio.keySet())
+            System.out.println("Stock: " + stock.getName() + ", Symbol: " + stock.getSymbol() + ", Price: " + stock.getPrice() + ", Quantity: " + portfolio.get(stock));
+    }
+}
